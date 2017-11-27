b2b3ee09a72100d8a19f2d1ed0b41ff54ec9c549
Kaiser <you@example.com>
2017-11-27 23:08:12 +0800

    first time

--- ch2/libHelloSLAM.cpp
+++ ch2/libHelloSLAM.cpp
@@ -1,4 +1,4 @@
-//这是一个库文件
+//这是一个库文件，库文件没有main函数，无法生成可执行程序
 #include <iostream>
 using namespace std;
 
--- ch2/useHello.cpp
+++ ch2/useHello.cpp
@@ -1,3 +1,4 @@
+// 自己写的头文件及非标准库的头文件都用“ ”，标准库的头文件用< >
 #include "libHelloSLAM.h"
 
 // 使用 libHelloSLAM.h 中的 printHello() 函数
